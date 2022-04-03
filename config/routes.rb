2bcb0c5d306d5206d169172586a2bb69ce29b3c5
bonsaiworks8 <bonsaiworks8@gmail.com>
2022-04-04 07:36:03 +0900

    不要なファイル・不要なコードの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   root to: "books#index"
   devise_for :users
-  resources :users, :only => [:index, :show]
+  resources :users, only: [:index, :show]
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
