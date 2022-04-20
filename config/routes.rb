2156fb5059270e0d94e79c950cf1eb0ae981f42e
bonsaiworks8 <bonsaiworks8@gmail.com>
2022-04-21 07:50:33 +0900

    ルーティングを見やすくまとめる・それに伴う変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
   resources :books
-  get 'users/:id/followings', to: 'binding_users#followings', as: :followings
-  get 'users/:id/followers', to: 'binding_users#followers' , as: :followers
-  post 'users/:id/followings', to: 'binding_users#create', as: :follow
-  delete 'users/:id/followings', to: 'binding_users#destroy', as: :unfollow
+  resources :users do
+    member do
+      get :followings, to: 'binding_users#followings'
+      get :followers, to: 'binding_users#followers'
+      post :followings, to: 'binding_users#create'
+      delete :followings, to: 'binding_users#destroy'
+    end
+  end
   resources :users, only: %i(index show)
 end
